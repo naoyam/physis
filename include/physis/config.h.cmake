c720a4ae8e4e19d8442331de3b7876c7fd9a18a9
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-09-14 02:49:37 +0900

    propagate some CMake variables to the installed physis header files

--- include/physis/config.h.cmake
+++ include/physis/config.h.cmake
@@ -1,6 +1,15 @@
 #ifndef PHYSIS_CONFIG_H_
 #define PHYSIS_CONFIG_H_
 
+// These are duplicates of common/config.h, but need to be here since
+// common/config.h is not going to be installed, while this header file
+// is installed by make install.
+#cmakedefine CUDA_ENABLED
+#cmakedefine MPI_ENABLED
+#cmakedefine PS_DEBUG
+#cmakedefine PS_VERBOSE_DEBUG
+#cmakedefine PS_VERBOSE
+
 #cmakedefine AUTO_DOUBLE_BUFFERING
 
 #endif /* PHYSIS_CONFIG_H_ */
