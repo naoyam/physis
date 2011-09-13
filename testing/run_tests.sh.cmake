86916383ff8fadc0800de27a9802b845d7b9d994
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-09-14 02:30:39 +0900

    new: compile test

--- testing/run_tests.sh.cmake
+++ testing/run_tests.sh.cmake
@@ -14,9 +14,12 @@
 ###############################################################
 WD=$PWD/test_output
 FLAG_KEEP_OUTPUT=1
+if [ "x$CFLAGS" = "x" ]; then
+	CFLAGS=""
+fi
 ###############################################################
 set -u
-set -e
+#set -e
 TIMESTAMP=$(date +%m-%d-%Y_%H-%M-%S)
 LOGFILE=$PWD/run_tests.log.$TIMESTAMP
 WD=$WD/$TIMESTAMP
@@ -24,21 +27,79 @@ ORIGINAL_DIRECTORY=$PWD
 mkdir -p $WD
 cd $WD
 
-NUM_SUCCESS=0
-NUM_FAIL=0
+NUM_SUCCESS_TRANS=0
+NUM_FAIL_TRANS=0
+NUM_SUCCESS_COMPILE=0
+NUM_FAIL_COMPILE=0
+NUM_SUCCESS_EXEC=0
+NUM_FAIL_EXEC=0
 
 function finish()
 {
-	echo "All tests completed. #SUCCESS: $NUM_SUCCESS, #FAIL: $NUM_FAIL."
+	echo "All tests completed."
+	echo  "[TRANSLATE] #SUCCESS: $NUM_SUCCESS_TRANS, #FAIL: $NUM_FAIL_TRANS."
+	echo  "[COMPILE]   #SUCCESS: $NUM_SUCCESS_COMPILE, #FAIL: $NUM_FAIL_COMPILE."
+	echo  "[EXECUTE]   #SUCCESS: $NUM_SUCCESS_EXEC, #FAIL: $NUM_FAIL_EXEC."
 	cd $ORIGINAL_DIRECTORY
 	if [ $FLAG_KEEP_OUTPUT -eq 0 ]; then
 		rm -rf $WD
 	fi
-	if [ $NUM_FAIL -eq 0 ]; then
+	if [ $NUM_FAIL_TRANS -eq 0 -a $NUM_FAIL_COMPILE -eq 0 -a \
+		$NUM_FAIL_EXEC -eq 0 ]; then
 		exit 0
 	else
 		exit 1
 	fi
+}
+
+function compile()
+{
+	src=$1
+	target=$2
+	echo compilation of $src for $target
+	src_file=$(basename $src .c).$target
+	echo $src_file
+	if [ "${MPI_ENABLED}" = "TRUE" ]; then
+		MPI_C_FLAGS="-pthread"
+		for mpiinc in $(echo "${MPI_INCLUDE_PATH}" | sed 's/;/ /g'); do
+			MPI_C_FLAGS+=" -I$mpiinc"
+		done
+	fi
+	case $target in
+		ref)
+			src_file+=".c"
+			cc -c $src_file -I${CMAKE_SOURCE_DIR}/include $CFLAGS
+			;;
+		cuda)
+			if [ "${CUDA_ENABLED}" != "TRUE" ]; then
+				echo "[COMPILE] Skipping CUDA compilation (not supported)"
+				return 0
+			fi
+			src_file+=".cu"
+			nvcc -c $src_file -I${CMAKE_SOURCE_DIR}/include -Xcompiler $CFLAGS
+			;;
+		mpi)
+			if [ "${MPI_ENABLED}" != "TRUE" ]; then
+				echo "[COMPILE] Skipping MPI compilation (not supported)"
+				return 0
+			fi
+			src_file+=".c"
+			cc -c $src_file -I${CMAKE_SOURCE_DIR}/include $MPI_C_FLAGS $CFLAGS
+			;;
+		mpi-cuda)
+			if [ "${MPI_ENABLED}" != "TRUE" -o "${CUDA_ENABLED}" != "TRUE" ]; then
+				echo "[COMPILE] Skipping MPI-CUDA compilation (not supported)"
+				return 0
+			fi
+			src_file+=".cu"
+			nvcc -c $src_file -I${CMAKE_SOURCE_DIR}/include \
+				$MPI_C_FLAGS -Xcompiler $CFLAGS 
+			;;
+		*)
+			echo "Error: Unsupported target"
+			finish
+			;;
+	esac
 }
 
 {
@@ -51,15 +112,25 @@ function finish()
 	for TARGET in $TARGETS; do
 		for TEST in $TESTS; do
 			SHORTNAME=$(basename $TEST)
-			echo "[TRANSLATION] Processing $SHORTNAME for $TARGET target"
+			echo "[TRANSLATE] Processing $SHORTNAME for $TARGET target"
 			if $PHYSISC --$TARGET -I${CMAKE_SOURCE_DIR}/include $TEST \
 				> $(basename $TEST).$TARGET.log 2>&1; then
-				echo "[TRANSLATION] SUCCESS"
-				NUM_SUCCESS=$(($NUM_SUCCESS + 1))
+				echo "[TRANSLATE] SUCCESS"
+				NUM_SUCCESS_TRANS=$(($NUM_SUCCESS_TRANS + 1))
 			else
-				echo "[TRANSLATION] FAIL"
-				NUM_FAIL=$(($NUM_FAIL + 1))
-			# Terminates immediately when failed
+				echo "[TRANSLATE] FAIL"
+				NUM_FAIL_TRANS=$(($NUM_FAIL_TRANS + 1))
+			    # Terminates immediately when failed
+				finish
+			fi
+			echo "[COMPILE] Processing $SHORTNAME for $TARGET target"
+			if compile $SHORTNAME $TARGET; then
+				echo "[COMPILE] SUCCESS"
+				NUM_SUCCESS_COMPILE=$(($NUM_SUCCESS_COMPILE + 1))
+			else
+				echo "[COMPILE] FAIL"
+				NUM_FAIL_COMPILE=$(($NUM_FAIL_COMPILE + 1))
+				# Terminates immediately when failed
 				finish
 			fi
 		done
