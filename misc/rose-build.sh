f7e55d437b1f9ac4d2178e3931f8b88e070003ef
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-09-16 18:38:29 +0900

    fix rose build script

--- misc/rose-build.sh
+++ misc/rose-build.sh
@@ -64,6 +64,7 @@ if [ -z "$JAVA_HOME" ]; then
 		linux*)
 			LDFLAGS=""
 			for i in $(locate libjvm.so); do
+				if echo $i | grep --silent -e gcc -e gcj -e openjdk; then continue; fi
 				echo -n "Using $i? [Y/n] "
 				read yn
 				if [ "$yn" != "n" ]; then
@@ -120,32 +121,54 @@ esac
 
 echo $NUM_PROCESSORS cores detected
 
+function exec_configure()
+{
+	echo $ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --disable-binary-analysis-tests -with-haskell=no $CONFIG_OPTIONS		
+	echo -n "Type Enter to proceed: "
+	read x
+	$ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --enable-binary-analysis-tests=no --disable-projects-directory --disable-tutorial-directory -with-haskell=no $CONFIG_OPTIONS
+	if [ $? == 0 ]; then
+		echo "Rerun again and select make for building ROSE"
+	fi
+}
+
+function exec_make()
+{
+	echo building ROSE by make -j$((NUM_PROCESSORS / 2))
+	make -j$((NUM_PROCESSORS / 2))
+	if [ $? == 0 ]; then
+		echo "Rerun again and select make install to install ROSE"
+	fi
+}
+
+function exec_install()
+{
+	echo installing ROSE
+	make install
+}
+
 echo "Commands"
 echo "1: configure"
 echo "2: make"
 echo "3: make install"
-echo -n "What to do? [1-3] "
+echo "4: configure && make && make install"
+echo -n "What to do? [1-4] (default: 4): "
 read command
+if [ "x$command" = "x" ]; then command="4"; fi
 case $command in
 	1)
-		echo Configuring ROSE: $ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,binaries --enable-cuda --enable-opencl --disable-binary-analysis-tests  $CONFIG_OPTIONS
-		echo -n "Type Enter to proceed: "
-		read x
-		$ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,binaries --enable-cuda --enable-opencl --disable-binary-analysis-tests $CONFIG_OPTIONS
-		if [ $? == 0 ]; then
-			echo "Rerun again and select make for building ROSE"
-		fi
+		exec_configure
 		;;
 	2)
-		echo building ROSE by make -j$((NUM_PROCESSORS / 2))
-		make -j$((NUM_PROCESSORS / 2))
-		if [ $? == 0 ]; then
-			echo "Rerun again and select make install to install ROSE"
-		fi
+		exec_make
 		;;
 	3)
-		echo installing ROSE
-		make install
+		exec_install
+		;;
+	4)
+		exec_configure
+		exec_make
+		exec_install
 		;;
 	*)
 		echo Invalid input \"$command\"
