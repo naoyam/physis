ba1fece00d37ba76848d25a38b50390426822b12
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-09-17 10:10:01 +0900

    new: automatically download ROSE src package

--- misc/rose-build.sh
+++ misc/rose-build.sh
@@ -3,7 +3,7 @@
 # Detects options and path settings and runs configure and make.
 # 
 # Usage:
-# rose-build.sh <src-path> <install-path>:
+# rose-build.sh -r <src-path> -p <install-path>:
 # - src-path: unpacked ROSE source directory
 # - install-path: installation path (should be an absolute path)
 #
@@ -14,6 +14,129 @@
 
 #set -u
 set -e
+
+ROSE_SRC=""
+INSTALL_PREFIX=""
+
+function download_latest_tarball()
+{
+	local site='https://outreach.scidac.gov'
+	echo "Trying to detect the latest ROSE source package..." 
+	local path=$(wget --quiet -O- "$site/frs/?group_id=24" | egrep -o -m1 '/frs/download.php/[0-9]+/rose-0.9.5a-without-EDG-[0-9]+\.tar\.gz')
+	echo "Downloading $site$path..."
+	wget $site$path
+	echo "Download finished."
+	tar zxf $(basename $path)
+	local dname=$(basename $path .tar.gz| sed 's/-without-EDG//')
+	mv  $dname src
+	mkdir $dname
+	mv src $dname
+	mkdir $dname/build $dname/install
+	ROSE_SRC=$(pwd)/$dname/src
+	INSTALL_PREFIX=$(pwd)/$dname/install
+	cd $dname/build	
+}
+
+function detect_boost()
+{
+	local BOOST_CANDIDATES=($HOME/homebrew /usr
+		$HOME/tools/boost /work0/GSIC/apps/boost/1_45_0/gcc)
+	if [ -z "$BOOST" ]; then
+		for c in ${BOOST_CANDIDATES[*]}; do
+			if [ -d $c/include/boost ]; then
+				BOOST=$c
+				break
+			fi
+		done
+	fi
+	echo Using Boost found at $BOOST
+}
+
+function set_java_home()
+{
+	if [ -z "$JAVA_HOME" ]; then
+		case $OSTYPE in
+			linux*)
+				for i in $(locate libjvm.so); do
+					if echo $i | grep --silent -e gcc -e gcj -e openjdk; then continue; fi
+					echo -n "Using $i? [Y/n] "
+					read yn
+					if [ "$yn" != "n" ]; then
+						export JAVA_HOME=${i%/jre*}
+						break
+					fi
+				done
+				
+				if [ -z "$JAVA_HOME" ]; then
+					echo "Error: no Java found"
+					exit 1
+				fi
+				;;
+			darwin*)
+				JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK
+				;;
+		esac
+	fi
+	echo JAVA_HOME is set to $JAVA_HOME
+}
+
+function detect_java_libraries()
+{
+	local JVM_DIR=$(dirname $(find ${JAVA_HOME}/ -name "libjvm\.*" | head -1))
+	case $OSTYPE in
+		linux*)
+			JAVA_LIBRARIES=${JAVA_HOME}/jre/lib:$JVM_DIR
+			;;
+		darwin*)
+			JAVA_LIBRARIES=$JVM_DIR
+			;;
+	esac
+}
+
+function exec_configure()
+{
+	if [ -z "$ROSE_SRC" ]; then echo "ERROR! ROSE src path not set"; exit 1; fi
+	if [ -z "$INSTALL_PREFIX" ]; then echo "ERROR! install prefix not set"; exit 1; fi	
+	echo Building ROSE at $ROSE_SRC and installing it to $INSTALL_PREFIX
+	echo $ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --disable-binary-analysis-tests -with-haskell=no 
+	echo -n "Type Enter to proceed: "
+	read x
+	$ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --enable-binary-analysis-tests=no --disable-projects-directory --disable-tutorial-directory -with-haskell=no 
+	if [ $? == 0 ]; then
+		echo "Rerun again and select make for building ROSE"
+	fi
+}
+
+function detect_num_cores()
+{
+	NUM_PROCESSORS=1 # default
+	echo "Detecting number of cores..."
+	case $OSTYPE in
+		linux*)
+			NUM_PROCESSORS=$(grep processor /proc/cpuinfo|wc -l)
+			;;
+		darwin*)
+			NUM_PROCESSORS=$(system_profiler |grep 'Total Number Of Cores' | awk '{print $5}')
+			;;
+	esac
+}
+
+function exec_make()
+{
+	detect_num_cores
+	echo building ROSE by make -j$NUM_PROCESSORS
+	make -j$((NUM_PROCESSORS / 2))
+	if [ $? == 0 ]; then
+		echo "Rerun again and select make install to install ROSE"
+	fi
+}
+
+function exec_install()
+{
+	echo installing ROSE
+	make install
+	echo "ROSE is installed at $INSTALL_PREFIX"
+}
 
 while getopts ":r:p:b:" opt; do
 	case $opt in
@@ -32,141 +155,45 @@ while getopts ":r:p:b:" opt; do
 	esac
 done
 
-shift $(($OPTIND - 1))
+detect_boost
+set_java_home
+detect_java_libraries
 
-if [ -z "$ROSE_SRC" ]; then
-	echo -n "ROSE source path: "
-	read ROSE_SRC
-fi
-if [ -z "$INSTALL_PREFIX" ]; then
-	echo -n "Install prefix path: "
-	read INSTALL_PREFIX
-fi
-
-echo Building ROSE at $ROSE_SRC and installing it to $INSTALL_PREFIX
-CONFIG_OPTIONS=$*
-
-BOOST_CANDIDATES=($HOME/homebrew /usr $HOME/tools/boost /work0/GSIC/apps/boost/1_45_0/gcc)
-
-if [ -z "$BOOST" ]; then
-	for c in ${BOOST_CANDIDATES[*]}; do
-		if [ -d $c/include/boost ]; then
-			BOOST=$c
-			break
-		fi
-	done
-fi
-
-echo Using Boost found at $BOOST
-
-if [ -z "$JAVA_HOME" ]; then
-	case $OSTYPE in
-		linux*)
-			LDFLAGS=""
-			for i in $(locate libjvm.so); do
-				if echo $i | grep --silent -e gcc -e gcj -e openjdk; then continue; fi
-				echo -n "Using $i? [Y/n] "
-				read yn
-				if [ "$yn" != "n" ]; then
-					export JAVA_HOME=${i%/jre*}
-					break
-				fi
-			done
-			
-			if [ -z "$JAVA_HOME" ]; then
-				echo "Error: no Java found"
-				exit 1
-			fi
-			;;
-		darwin*)
-			JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK
-			;;
-	esac
-fi
-
-JVM_DIR=$(dirname $(find ${JAVA_HOME}/ -name "libjvm\.*" | head -1))
 case $OSTYPE in
 	linux*)
-		JAVA_LIBRARIES=${JAVA_HOME}/jre/lib:$JVM_DIR
-		;;
-	darwin*)
-		JAVA_LIBRARIES=$JVM_DIR
-		;;
-esac
-
-# Is this necessary?
-#export LDFLAGS="-Wl,-rpath=${JAVA_LIBRARIES}:${BOOST}/lib"
-case $OSTYPE in
-	linux*)
+		echo export LD_LIBRARY_PATH=${JAVA_LIBRARIES}:${BOOST}/lib:$LD_LIBRARY_PATH		
 		export LD_LIBRARY_PATH=${JAVA_LIBRARIES}:${BOOST}/lib:$LD_LIBRARY_PATH
 		;;
 	darwin*)
+		echo export DYLD_LIBRARY_PATH=${JAVA_LIBRARIES}:${BOOST}/lib:$DYLD_LIBRARY_PATH		
 		export DYLD_LIBRARY_PATH=${JAVA_LIBRARIES}:${BOOST}/lib:$DYLD_LIBRARY_PATH
 		;;
 esac
 
-echo LDFLAGS is set to $LDFLAGS
-echo LD_LIBRARY_PATH is set to $LD_LIBRARY_PATH
-echo JAVA_HOME is set to $JAVA_HOME
-echo set LD_LIBRARY_PATH to $LD_LIBRARY_PATH
-
-echo "Detecting number of cores..."
-case $OSTYPE in
-	linux*)
-		NUM_PROCESSORS=$(grep processor /proc/cpuinfo|wc -l)
-		;;
-	darwin*)
-		NUM_PROCESSORS=$(system_profiler |grep 'Total Number Of Cores' | awk '{print $5}')
-		;;
-esac
-
-echo "$NUM_PROCESSORS cores detected"
-
-function exec_configure()
-{
-	echo $ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --disable-binary-analysis-tests -with-haskell=no $CONFIG_OPTIONS		
-	echo -n "Type Enter to proceed: "
-	read x
-	$ROSE_SRC/configure --prefix=$INSTALL_PREFIX --with-CXX_DEBUG=-g --with-CXX_WARNINGS="-Wall -Wno-deprecated" --with-boost=$BOOST --enable-languages=c,c++,fortran,cuda,opencl --enable-binary-analysis-tests=no --disable-projects-directory --disable-tutorial-directory -with-haskell=no $CONFIG_OPTIONS
-	if [ $? == 0 ]; then
-		echo "Rerun again and select make for building ROSE"
-	fi
-}
-
-function exec_make()
-{
-	echo building ROSE by make -j$((NUM_PROCESSORS / 2))
-	make -j$((NUM_PROCESSORS / 2))
-	if [ $? == 0 ]; then
-		echo "Rerun again and select make install to install ROSE"
-	fi
-}
-
-function exec_install()
-{
-	echo installing ROSE
-	make install
-}
-
 echo "Commands"
-echo "1: configure"
-echo "2: make"
-echo "3: make install"
-echo "4: configure && make && make install"
-echo -n "What to do? [1-4] (default: 4): "
+echo "1: download"
+echo "2: configure"
+echo "3: make"
+echo "4: make install"
+echo "5: do all"
+echo -n "What to do? [1-5] (default: 5): "
 read command
-if [ "x$command" = "x" ]; then command="4"; fi
+if [ "x$command" = "x" ]; then command="5"; fi
 case $command in
 	1)
-		exec_configure
+		download_latest_tarball
 		;;
 	2)
-		exec_make
+		exec_configure
 		;;
 	3)
-		exec_install
+		exec_make
 		;;
 	4)
+		exec_install
+		;;
+	5)
+		download_latest_tarball
 		exec_configure
 		exec_make
 		exec_install
