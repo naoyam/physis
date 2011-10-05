40eea5b55177c5e82b6eeb252fdf20e0fa2a47d6
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-10-05 15:51:25 +0900

    new test code

--- testing/tests/test_5.c
+++ testing/tests/test_5.c
@@ -1,23 +1,30 @@
+/*
+ * Test: Using two kernels 
+ */ 
+
 #include <stdio.h>
 #include "physis/physis.h"
 
 #define N 8
 
-void kernel1(const int x, const int y, const int z, PSGrid3DFloat g) {
-  float v = PSGridGet(g, x, y, z) * 2;
-  PSGridEmit(g, v);
+void kernel1(const int x, const int y, const int z,
+             PSGrid3DFloat g1, PSGrid3DFloat g2) {
+  float v = PSGridGet(g1, x, y, z) * 2;
+  PSGridEmit(g2, v);
   return;
 }
 
-void kernel2(const int x, const int y, const int z, PSGrid3DFloat g) {
-  float v = PSGridGet(g, x, y, z) / 2;
-  PSGridEmit(g, v);
+void kernel2(const int x, const int y, const int z,
+             PSGrid3DFloat g2, PSGrid3DFloat g1) {
+  float v = PSGridGet(g2, x, y, z) / 2;
+  PSGridEmit(g1, v);
   return;
 }
 
 int main(int argc, char *argv[]) {
   PSInit(&argc, &argv, 3, N, N, N);
-  PSGrid3DFloat g = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g1 = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g2 = PSGrid3DFloatNew(N, N, N);  
   PSDomain3D d = PSDomain3DNew(0, N, 0, N, 0, N);
   size_t nelms = N*N*N;
   
@@ -28,21 +35,23 @@ int main(int argc, char *argv[]) {
   }
   float *outdata = (float *)malloc(sizeof(float) * nelms);
     
-  PSGridCopyin(g, indata);
+  PSGridCopyin(g1, indata);
 
-  PSStencilRun(PSStencilMap(kernel1, d, g),
-               PSStencilMap(kernel2, d, g));
+  PSStencilRun(PSStencilMap(kernel1, d, g1, g2),
+               PSStencilMap(kernel2, d, g2, g1));
     
-  PSGridCopyout(g, outdata);
+  PSGridCopyout(g1, outdata);
     
   for (i = 0; i < nelms; i++) {
     if (indata[i] != outdata[i]) {
       fprintf(stderr, "Error: mismatch at %d, in: %f, out: %f\n",
               i, indata[i], outdata[i]);
+      exit(1);
     }
   }
 
-  PSGridFree(g);
+  PSGridFree(g1);
+  PSGridFree(g2);  
   PSFinalize();
   free(indata);
   free(outdata);
