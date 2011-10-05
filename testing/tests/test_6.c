be52d42ac33fdb19ec0208cd95581b78f186abb4
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-10-05 15:58:01 +0900

    new test

--- testing/tests/test_6.c
+++ testing/tests/test_6.c
@@ -3,12 +3,13 @@
 
 #define N 8
 
-void kernel1(const int x, const int y, const int z, PSGrid3DFloat g) {
-  float v = PSGridGet(g, x, y, z) +
-      PSGridGet(g, x+1, y, z) + PSGridGet(g, x-1, y, z) +
-      PSGridGet(g, x, y+1, z) + PSGridGet(g, x, y-1, z) +
-      PSGridGet(g, x, y, z-1) + PSGridGet(g, x, y, z+1);
-  PSGridEmit(g, v);
+void kernel1(const int x, const int y, const int z,
+             PSGrid3DFloat g1, PSGrid3DFloat g2) {
+  float v = PSGridGet(g1, x, y, z) +
+      PSGridGet(g1, x+1, y, z) + PSGridGet(g1, x-1, y, z) +
+      PSGridGet(g1, x, y+1, z) + PSGridGet(g1, x, y-1, z) +
+      PSGridGet(g1, x, y, z-1) + PSGridGet(g1, x, y, z+1);
+  PSGridEmit(g2, v);
   return;
 }
 
@@ -36,6 +37,7 @@ void check(float *input, float *output) {
     if (input[i] != output[i]) {
       printf("Mismatch at %d: %f != %f\n", i,
              input[i], output[i]);
+      exit(1);
     }
   }
 }
@@ -44,11 +46,13 @@ void check(float *input, float *output) {
 
 int main(int argc, char *argv[]) {
   PSInit(&argc, &argv, 3, N, N, N);
-  PSGrid3DFloat g = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g1 = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g2 = PSGrid3DFloatNew(N, N, N);  
 
   PSDomain3D d = PSDomain3DNew(0+halo_width, N-halo_width,
                                0+halo_width, N-halo_width,
-                               0+halo_width, N-halo_width);                          size_t nelms = N*N*N;
+                               0+halo_width, N-halo_width);
+  size_t nelms = N*N*N;
   
   float *indata = (float *)malloc(sizeof(float) * nelms);
   int i;
@@ -58,22 +62,25 @@ int main(int argc, char *argv[]) {
   float *outdata_ps = (float *)malloc(sizeof(float) * nelms);
   float *outdata = (float *)malloc(sizeof(float) * nelms);  
     
-  PSGridCopyin(g, indata);
+  PSGridCopyin(g1, indata);
   
   int iter = 1 * 2;
   
-  PSStencilRun(PSStencilMap(kernel1, d, g), iter);
+  PSStencilRun(PSStencilMap(kernel1, d, g1, g2),
+               PSStencilMap(kernel1, d, g2, g1),
+               iter);
 
-  for (i = 0 ; i < iter; i+=2) {
+  for (i = 0 ; i < iter; ++i) {
     kernel_ref(indata, outdata, halo_width);
     kernel_ref(outdata, indata, halo_width);
   }
     
-  PSGridCopyout(g, outdata_ps);
+  PSGridCopyout(g1, outdata_ps);
 
   check(indata, outdata_ps);
 
-  PSGridFree(g);
+  PSGridFree(g1);
+  PSGridFree(g2);  
   PSFinalize();
   free(indata);
   free(outdata);
