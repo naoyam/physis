f5a9940ca3de00e75834231f56a8f8d090660223
Naoya Maruyama <naoya@matsulab.is.titech.ac.jp>
2011-10-28 11:10:19 +0900

    Add test_4 manual code with a small fix of the Physis implementation.

--- testing/tests/test_4.c
+++ testing/tests/test_4.c
@@ -7,41 +7,28 @@
 
 #define N 8
 
-void kernel1(const int x, const int y, const int z, PSGrid3DFloat g,
-             PSGrid3DFloat g2) {
+void kernel(const int x, const int y, const int z, PSGrid3DFloat g,
+            PSGrid3DFloat g2) {
   float v = PSGridGet(g, x, y, z) +
-      PSGridGet(g, x+1, y+1, z) + PSGridGet(g, x-1, y, z) +
+      PSGridGet(g, x+1, y, z) + PSGridGet(g, x-1, y, z) +
       PSGridGet(g, x, y+1, z) + PSGridGet(g, x, y-1, z) +
       PSGridGet(g, x, y, z-1) + PSGridGet(g, x, y, z+1);
   PSGridEmit(g2, v);
   return;
 }
 
-#define GET(g, x, y, z) g[(x) + (y) * N + (z) * N * N]
-
-void check(float *input, float *output, int halo_width) {
-  int i, j, k;
-  for (i = halo_width; i < N-halo_width; ++i) {
-    for (j = halo_width; j < N-halo_width; ++j) {
-      for (k = halo_width; k < N-halo_width; ++k) {
-        float v = GET(input, i, j, k) +
-            GET(input, i-1, j, k) + GET(input, i+1, j+1, k) +
-            GET(input, i, j-1, k) + GET(input, i, j+1, k) +
-            GET(input, i, j, k-1) + GET(input, i, j, k+1);
-        if (v != GET(output, i, j, k)) {
-          printf("ERROR at (%d, %d, %d): %f != %f\n",
-                 i, j, k, v, GET(output, i, j, k));
-          exit(1);
-        }
-      }
-    }
+void dump(float *input) {
+  int i;
+  for (i = 0; i < N*N*N; ++i) {
+    printf("%f\n", input[i]);
   }
 }
+
 #define halo_width (1)
 
 int main(int argc, char *argv[]) {
   PSInit(&argc, &argv, 3, N, N, N);
-  PSGrid3DFloat g = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g1 = PSGrid3DFloatNew(N, N, N);
   PSGrid3DFloat g2 = PSGrid3DFloatNew(N, N, N);
 
   PSDomain3D d = PSDomain3DNew(0+halo_width, N-halo_width,
@@ -56,15 +43,15 @@ int main(int argc, char *argv[]) {
   }
   float *outdata = (float *)malloc(sizeof(float) * nelms);
     
-  PSGridCopyin(g, indata);
+  PSGridCopyin(g1, indata);
+  PSGridCopyin(g2, indata);  
 
-  PSStencilRun(PSStencilMap(kernel1, d, g, g2));
+  PSStencilRun(PSStencilMap(kernel, d, g1, g2));
     
   PSGridCopyout(g2, outdata);
+  dump(outdata);  
 
-  check(indata, outdata, halo_width);
-
-  PSGridFree(g);
+  PSGridFree(g1);
   PSGridFree(g2);
   PSFinalize();
   free(indata);
