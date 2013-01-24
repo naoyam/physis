89f66be2eb33e9d67e6739b5bc822bfe9d3e3bac
Naoya Maruyama <nmaruyama@riken.jp>
2013-01-24 05:33:25 -0500

    FIX: guard macro typo

--- runtime/rpc_mpi_opencl.h
+++ runtime/rpc_mpi_opencl.h
@@ -1,5 +1,5 @@
 #ifndef PHYSIS_RUNTIME_RPC_MPI_OPENCL_H_
-#define PHYSIS_RUNTIME_RPC_MPI_CUDA_H_
+#define PHYSIS_RUNTIME_RPC_MPI_OPENCL_H_
 
 #include "runtime/runtime_common.h"
 #include "runtime/rpc_mpi.h"
