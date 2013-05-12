6319c8c9f4594285c62c655e8bc4fbd14f31abe0
Naoya Maruyama <nmaruyama@riken.jp>
2013-05-12 22:45:39 +0900

    WIP

---
+++ translator/physis_names.h
@@ -0,0 +1,14 @@
+// Copyright 2011-2012, RIKEN AICS.
+// All rights reserved.
+//
+// This file is distributed under the BSD license. See LICENSE.txt for
+// details.
+#ifndef PHYSIS_TRANSLATOR_PHYSIS_NAMES_H_
+#define PHYSIS_TRANSLATOR_PHYSIS_NAMES_H_
+
+#define PS_GRID_EMIT_NAME "emit"
+#define PS_GRID_EMIT_UTYPE_NAME "__PSGridEmitUtype"
+
+#endif /* PHYSIS_TRANSLATOR_PHYSIS_NAMES_H_ */
+
+
