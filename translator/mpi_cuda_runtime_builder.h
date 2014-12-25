1a32991b84d844edb9d43c7920fb2937f7a79f6c
Naoya Maruyama <nmaruyama@riken.jp>
2014-12-25 19:23:11 +0900

    Fix MPI-CUDA translator translation executon errors
    Note translation still does not produce correct code. Needs to fix the interface with the new runtime.

--- translator/mpi_cuda_runtime_builder.h
+++ translator/mpi_cuda_runtime_builder.h
@@ -1,16 +1,11 @@
-// Copyright 2011, Tokyo Institute of Technology.
-// All rights reserved.
-//
-// This file is distributed under the license described in
-// LICENSE.txt.
-//
-// Author: Naoya Maruyama (naoya@matsulab.is.titech.ac.jp)
+// Licensed under the BSD license. See LICENSE.txt for more details.
 
 #ifndef PHYSIS_TRANSLATOR_MPI_CUDA_RUNTIME_BUILDER_H_
 #define PHYSIS_TRANSLATOR_MPI_CUDA_RUNTIME_BUILDER_H_
 
 #include "translator/translator_common.h"
 #include "translator/mpi_runtime_builder.h"
+#include "translator/cuda_runtime_builder.h"
 
 namespace physis {
 namespace translator {
@@ -25,7 +20,27 @@ SgExpression *BuildStreamBoundaryKernel(int idx);
 class MPICUDARuntimeBuilder: public MPIRuntimeBuilder {
  public:
   MPICUDARuntimeBuilder(SgScopeStatement *global_scope):
-      MPIRuntimeBuilder(global_scope) {}
+      MPIRuntimeBuilder(global_scope),
+      cuda_rt_builder_(new CUDARuntimeBuilder(global_scope))
+  {}
+  
+  virtual ~MPICUDARuntimeBuilder() {
+    delete cuda_rt_builder_;
+  }
+
+  virtual SgExpression *BuildGridOffset(
+      SgExpression *gvref, int num_dim,
+      const SgExpressionPtrList *offset_exprs, bool is_kernel,
+      bool is_periodic,
+      const StencilIndexList *sil) {
+    return cuda_rt_builder_->BuildGridOffset(
+        gvref, num_dim, offset_exprs, is_kernel,
+        is_periodic, sil);
+  }
+
+  
+ protected:
+  CUDARuntimeBuilder *cuda_rt_builder_;
 };
 
 } // namespace translator
