55fbe59b270e7571f4ab1e29bedfdb3f9585c15a
Naoya Maruyama <nmaruyama@riken.jp>
2015-01-25 14:23:55 +0900

    Add a new test case for 27-pt stencil with reduction

---
+++ tests/system_tests/test_cases/test_27-pt-reduction.c
@@ -0,0 +1,69 @@
+/*
+ * TEST: 27-point stencil with reduction
+ * DIM: 3
+ * PRIORITY: 1 
+ */
+
+#include <stdio.h>
+#include "physis/physis.h"
+
+#define N 32
+
+void kernel(const int x, const int y, const int z, PSGrid3DFloat g,
+            PSGrid3DFloat g2) {
+  float v =
+      // z == -1
+      PSGridGet(g, x, y, z-1) + PSGridGet(g, x+1, y, z-1) +
+      PSGridGet(g, x-1, y, z-1) + PSGridGet(g, x, y+1, z-1) +
+      PSGridGet(g, x+1, y+1, z-1) + PSGridGet(g, x-1, y+1, z-1) +
+      PSGridGet(g, x, y-1, z-1) + PSGridGet(g, x+1, y-1, z-1) +
+      PSGridGet(g, x-1, y-1, z-1) +
+      // z == 0
+      PSGridGet(g, x, y, z) + PSGridGet(g, x+1, y, z) +
+      PSGridGet(g, x-1, y, z) + PSGridGet(g, x, y+1, z) +
+      PSGridGet(g, x+1, y+1, z) + PSGridGet(g, x-1, y+1, z) +
+      PSGridGet(g, x, y-1, z) + PSGridGet(g, x+1, y-1, z) +
+      PSGridGet(g, x-1, y-1, z) +
+      // z == 1
+      PSGridGet(g, x, y, z+1) + PSGridGet(g, x+1, y, z+1) +
+      PSGridGet(g, x-1, y, z+1) + PSGridGet(g, x, y+1, z+1) +
+      PSGridGet(g, x+1, y+1, z+1) + PSGridGet(g, x-1, y+1, z+1) +
+      PSGridGet(g, x, y-1, z+1) + PSGridGet(g, x+1, y-1, z+1) +
+      PSGridGet(g, x-1, y-1, z+1);
+  PSGridEmit(g2, v);
+  return;
+}
+
+#define halo_width (1)
+
+int main(int argc, char *argv[]) {
+  PSInit(&argc, &argv, 3, N, N, N);
+  PSGrid3DFloat g1 = PSGrid3DFloatNew(N, N, N);
+  PSGrid3DFloat g2 = PSGrid3DFloatNew(N, N, N);
+
+  PSDomain3D d = PSDomain3DNew(0+halo_width, N-halo_width,
+                               0+halo_width, N-halo_width,
+                               0+halo_width, N-halo_width);
+  size_t nelms = N*N*N;
+  
+  float *indata = (float *)malloc(sizeof(float) * nelms);
+  int i;
+  for (i = 0; i < nelms; i++) {
+    indata[i] = i;
+  }
+    
+  PSGridCopyin(g1, indata);
+  PSGridCopyin(g2, indata);  
+
+  PSStencilRun(PSStencilMap(kernel, d, g1, g2));
+  float v;
+  PSReduce(&v, PS_SUM, g2);
+  printf("%f\n", v);
+
+  PSGridFree(g1);
+  PSGridFree(g2);
+  PSFinalize();
+  free(indata);
+  return 0;
+}
+
